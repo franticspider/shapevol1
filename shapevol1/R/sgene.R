d8544f62dfb5f1a0089dad5cecf944ee4f114953
Simon Hickinbotham <sjh518@york.ac.uk>
2019-11-29 10:04:39 +0000

    Working version in notebooks/091111Progress.Rmd

--- shapevol1/R/sgene.R
+++ shapevol1/R/sgene.R
@@ -10,10 +10,50 @@
 #' @param stop the stopping condition
 #' @param dom the dominance
 #' @return a dataframe containing the gene
+#' @export
 #' @examples
 #' g1 <- sgene("DirectionX",0,T,0,1.5,1)
 sgene <- function(att,valtyp,status=F,start,stop,dom){
-  return(data.frame(att=att,valtyp=valtyp,status=status,start,stop,dom))
+  return(data.frame(att=att,valtyp=valtyp,status=status,start,stop,dom,stringsAsFactors = F))
 }
 
 
+
+#' Create a position entry
+#' 
+#' @param X the X position
+#' @param Y the Y position
+#' @param Z the Z position
+#' @return a dataframe containing the position info
+#' @export
+#' @examples
+#' pp <- spos(0.0,0.0,0.1)
+spos <- function(X,Y,Z){
+  return(data.frame(X=X,Y=Y,Z=Z,stringsAsFactors = F))
+}
+
+
+#' Update an array of positions
+#'
+#' @pram pos an array of new positions
+#' @param X the X position
+#' @param Y the Y position
+#' @param Z the Z position
+#' @param err a tolerance value used to eliminate duplicates
+#' @return a dataframe containing the position info
+#' @export
+#' @examples
+#' pp <- spos(0.0,0.0,0.1)
+addpos <- function(X,Y,Z,pos=NULL,err=0.01){
+  tmp <- spos(X,Y,Z)
+  if(is.null(pos))
+    pos <- tmp
+  else{
+    elim <- data.frame(X = abs(pos$X-tmp$X), Y = abs(pos$Y-tmp$Y), Z = abs(pos$Z-tmp$Z))
+    elim <- elim[elim$X < err & elim$Y <err & elim$Z < err,]
+    
+    if(nrow(elim)==0)
+      pos <- rbind(pos,tmp)
+  }
+  return(pos)
+}
---
+++ shapevol1/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(shapevol1)
+
+test_check("shapevol1")
